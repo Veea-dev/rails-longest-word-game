8d3bb716215e605b0ac6703b6049102f8cd9cf21
Anna Eiden <annaverenaeiden@hotmail.de>
2022-02-14 16:59:13 +0100

    Half way

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,20 @@
+class GamesController < ApplicationController
+
+  def new
+    puts "Here is your grid:"
+    @grid = ('A'..'Z').to_a.sample(8).join("")
+    puts @grid
+  end
+
+  def score
+    @letters = @word.to_a
+    @letters.each do |letter|
+    if @letter
+    end
+  end
+end
+# check ist Buchstabe in dem Array enthalten
+# check ist Wort in API
+
+# runde funktion
+# eckige Array
